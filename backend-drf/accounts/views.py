f25f5017a3a50b7ab161ff9d9bd73b7d5dd61c88
Yogesh23-03 <s66909287@gmail.com>
2026-03-01 23:48:15 +0530

    completefrontback

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -3,9 +3,18 @@ from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]  # Change to IsAuthenticated for actual protection
+
+    def get(self, request):
+        return Response({"message": "This is a protected view!"})
--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,4 +136,7 @@ REST_FRAMEWORK = {
     )
 }
 
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
